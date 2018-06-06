edb69bf6b1edab0a38cff9202a82fa0afe134779
Jainesh J Hira <jaineshster@gmail.com>
2018-06-06 20:38:04 +0200

    Added some generic pyautogui to test on all OSes

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
-
+from testpyauto import TestPyAuto
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Home Page for Pyautogui Test'
 
-@app.route('/start')
+
+@app.route('/testpyauto/start')
 def start():
+    testpy = TestPyAuto()
+    testpy.startPyAuto()
     return 'Start Application'
---
+++ testpyauto.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+
+class TestPyAuto:
+
+    def startPyAuto(self):
+        print(pyautogui.size())
+        pyautogui.press('apps')
